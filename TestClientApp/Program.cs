07985cc09f093f7e4001e4aff48cac0fce31b64e
Mirian-hub <Net@fina2.net>
2020-05-17 22:18:50 +0400

    Add project files.

---
+++ TestClientApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClientApp.DigitalSignatureServRef;
+
+namespace TestClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DigitalSignatureServRef.DigitalSignatureServiceSoapClient soapClient = new DigitalSignatureServRef.DigitalSignatureServiceSoapClient();
+            string text = "some test text";
+            Task<SignitureResponse> res = soapClient.SignitureAsync("user123", text);
+            string finRes = res.Result.Body.SignitureResult;
+            Console.WriteLine(finRes);
+        }
+    }
+}
